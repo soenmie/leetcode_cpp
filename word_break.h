ad779aaf4ffee05a29c29633a4909e10e8e4c2c5
Mian Sun <svnmian@gmail.com>
2015-07-14 00:33:08 +0800

    modify word break

--- word_break.h
+++ word_break.h
@@ -3,9 +3,7 @@ public:
 	bool wordBreak(string s, unordered_set<string>& wordDict) {
 		int n = s.size();
 		bool dp[n + 1];
-		for (int i = 0; i <= n; ++i) {
-			dp[i] = false;
-		}
+		memset(dp, false, sizeof(dp));
 		dp[0] = true;
 		for (int i = 1; i <= n; ++i) {
 			for (int j = 0; j < i; ++j) {
